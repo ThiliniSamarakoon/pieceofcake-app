c7de8dfe1644a0626dd4a045c9aa5dd9c2dd36cf
Thilini Samarakoon <bhagyas-im19110@stu.kln.ac.lk>
2023-07-24 00:00:58 +0530

    Pay advance page calculation corrected

--- public/js/script_pay-advance.js
+++ public/js/script_pay-advance.js
@@ -3,14 +3,14 @@ function calculateRemainingAmount() {
     var dueAmountStr = document.getElementById('dueAmount').textContent;
     var payAmount = parseFloat(document.getElementById('payAmount').value);
 
-    // Remove any non-numeric characters from the due amount string
-    dueAmountStr = dueAmountStr.replace(/[^0-9\.]/g, '');
+    // Remove the "Rs." string from the due amount string
+    dueAmountStr = dueAmountStr.replace('Rs.', '');
 
-    // Convert the due amount string to a number
+    // Convert the remaining due amount string to a number
     var dueAmount = parseFloat(dueAmountStr);
 
     // Convert the pay amount to a number
-    var payAmount = parseFloat(payAmount);
+    payAmount = parseFloat(payAmount);
 
     // Calculate the remaining amount
     var remainingAmount = dueAmount - payAmount;
@@ -18,4 +18,5 @@ function calculateRemainingAmount() {
     // Update the remaining amount field with the calculated value
     document.getElementById('remainingAmount').value = remainingAmount.toFixed(2);
 }
+
 
